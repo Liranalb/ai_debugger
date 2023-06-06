0669266cd31b1c5ac3623b16b044d4f736cad541
Liranalb <liran.alb@gmail.com>
2023-06-06 13:56:25 +0300

    Add card to about page and add home button to navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,15 @@
 import strings from '../strings/about_strings.json'
+import style from './About.module.css'
+import Card from '../ui/Card';
 
 const About = () => {
-    return <div>{strings.about}</div>
+    return (
+        <Card>
+            <div className={style.textBox}>
+            <h1>About</h1>
+                {strings.about}</div>
+        </Card>
+    );
 }
 
 export default About;
