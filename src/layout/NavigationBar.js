91fd9b2295584bb711d5dc739a73a4569a9255f0
Liranalb <liran.alb@gmail.com>
2023-06-06 13:56:25 +0300

    Add card to about page and add home button to navbar

--- src/layout/NavigationBar.js
+++ src/layout/NavigationBar.js
@@ -10,13 +10,16 @@ const NavigationBar = () => {
             <nav>
                 <ul>
                     <li>
-                        <Link to={strings.navbar.links.about}>About</Link>
+                        <Link to={strings.navbar.links.home}>Home</Link>
                     </li>
                     <li>
                         <Link to={strings.navbar.links.github_address}>GitHub</Link>
                     </li>
                     <li>
                         <Link to={strings.navbar.links.linkedin_address}>Linkedin</Link>
+                    </li>
+                    <li>
+                        <Link to={strings.navbar.links.about}>About</Link>
                     </li>
                 </ul>
             </nav>
