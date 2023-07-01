2af0f2f80088e45ddc7771100b7c827d3789199e
Liranalb <liran.alb@gmail.com>
2023-07-01 19:41:27 +0300

    Initiate pipeline and add a simple test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders home page', () => {
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    // Assert that the Home component is rendered
+    const homeElement = screen.getByText(/This is the home page/i);
+    expect(homeElement).toBeInTheDocument();
+  });
+
+  test('renders about page', () => {
+    render(
+      <MemoryRouter initialEntries={['/about']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    // Assert that the About component is rendered
+    const aboutElement = screen.getByText(/This is the about page/i);
+    expect(aboutElement).toBeInTheDocument();
+  });
 });
