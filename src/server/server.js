bd8391069f82924fad110bea1b8b5f26d75bd419
Liran Alboher <37271751+Liranalb@users.noreply.github.com>
2023-07-15 12:23:33 +0300

    Feature/server (#6)
    * Add a prompt strings file
    
    * Implement promptGenerator()
    
    * Fix typo
    
    * Fix func name
    
    * Add prompt posting to openai from postPrompt()
    
    * Add nodemon
    
    * Change server's port, fix openai import and remove duplicate code
    
    * fix crash issue on post request and update async handling
    
    * Change config consts to uppercase
    
    * Add .env file support for the OpenAI key
    
    * replace api model to gpt-3.5-turbo and post api response to the frontend
    
    * Rename serverConfig and change prompt text
    
    * Fix .env bug
    
    * set port to be the port from .env file and replace hardcoded variables

--- src/server/server.js
+++ src/server/server.js
@@ -1,38 +1,49 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const config = require('./apiReqConfig.json');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
-
 app.use(bodyParser.json());
 app.use(cors());
 
+const port = process.env.DEV_PORT;
 const promptGenerator = (code, logs) => {
-    return "Building a prompt from the code and logs";
+    if (!code || !logs)
+        throw new Error("Code or Logs cannot be empty!");
+    const prompt = config.ACT.STEP_BY_STEP.concat(config.PRE_CODE_PROMPT, code, config.PRE_LOG_PROMPT, logs);
+    return prompt;
 };
 
-const promptResponse = (generatedPrompt) => {
-    return 'Sends the prompt to GPT API';
+const postPrompt = async (prompt) => {
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    const response = await openai.createChatCompletion({
+        model: config.GPT.MODEL,
+        messages: [{"role":config.ROLE,"content":prompt}],
+        temperature: config.GPT.TEMPERATURE,
+        max_tokens: config.GPT.MAX_TOKENS,
+        top_p: config.GPT.TOP_P,
+        frequency_penalty: config.GPT.FREQUENCY_PENALTY,
+        presence_penalty: config.GPT.PRESENCE_PENALTY,
+    }); 
+
+    return response.data.choices[0].message.content;
 };
 
-const pasrseResponse = (promptResponse) => {
-    return 'This is a parsed response from the backend...';
-};
-
-app.get('/', (req, res) => {
-    res.send('Hi!')
-})
-
-app.post('/post', (req, res) => {
+app.post('/post', async (req, res, next) => {
     try {
         const { code, logs } = req.body;
         const prompt = promptGenerator(code, logs);
-        const gptResponse = promptResponse(prompt);
-        const responseData = pasrseResponse(gptResponse);
+        const gptResponse = await postPrompt(prompt);
 
         const response = {
-            response: responseData,
+            response: gptResponse,
             createdAt: new Date().toISOString()
         };
         res.json(response);
@@ -41,7 +52,7 @@ app.post('/post', (req, res) => {
     }
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
     console.error(err);
     res.status(500).json({ error: 'Internal Server Error' });
 });
