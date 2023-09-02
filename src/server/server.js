31650155aab09e0918ecc428829ef066976f538e
Liran Alboher <37271751+Liranalb@users.noreply.github.com>
2023-09-02 09:33:53 +0300

    Refactor/server (#15)
    * Fix typo
    
    * Create serverUtils module for server's utility functions, remove redundant code and fix middleware error handling
    
    * Expand global error handling
    
    ---------
    
    Co-authored-by: Liran Alboher <liran@lirans-MacBook-Pro.local>

--- src/server/server.js
+++ src/server/server.js
@@ -1,54 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const config = require('./apiReqConfig.json'); // Load OpenAI configuration
-const { Configuration, OpenAIApi } = require("openai");
+const { promptGenerator, postPrompt } = require('./serverUtils');
 require('dotenv').config(); // Load environment variables from a .env file
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = process.env.DEV_PORT; // Get port from environment variables
+const port = process.env.DEV_PORT || 3001; // Get port from environment variables
 
-// generates the prompt for OpenAI based on code and logs
-const promptGenerator = (code, logs) => {
-    if (!code || !logs)
-        throw new Error("Code or Logs cannot be empty!");
-
-    // Concatenate prompts from configuration string and user inputs
-    const prompt = config.ACT.STEP_BY_STEP.concat(config.PRE_CODE_PROMPT, code, config.PRE_LOG_PROMPT, logs);
-    return prompt;
-};
-
-// Send a prompt to OpenAI and get a completion
-const postPrompt = async (prompt) => {
-    const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY, // Get API key from environment variables
-    });
-    const openai = new OpenAIApi(configuration);
-
-    // Create a chat completion request to OpenAI API
-    const response = await openai.createChatCompletion({
-        model: config.GPT.MODEL,
-        messages: [{"role":config.ROLE,"content":prompt}],
-        temperature: config.GPT.TEMPERATURE,
-        max_tokens: config.GPT.MAX_TOKENS,
-        top_p: config.GPT.TOP_P,
-        frequency_penalty: config.GPT.FREQUENCY_PENALTY,
-        presence_penalty: config.GPT.PRESENCE_PENALTY,
-    }); 
-
-    return response.data.choices[0].message.content;
-};
-
-app.post('/post', async (req, res) => {
+app.post('/post', async (req, res, next) => {
     try {
         const { code, logs } = req.body;
-        if (!code || !logs) {
-            throw new Error("Code or Logs cannot be empty!");
-        }
-
         const prompt = promptGenerator(code, logs); // Generate the prompt for OpenAI
         const gptResponse = await postPrompt(prompt); // Get completion from OpenAI
         
@@ -65,12 +28,20 @@ app.post('/post', async (req, res) => {
             createdAt: new Date().toISOString()
         };
         res.status(500).json(response);
-    }
+        next(error);
+    }   
 });
 
 app.use((err, req, res, next) => {
     console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    const errorMessage = "Internal Server Error";
+    const response = {
+        error: {
+            message: errorMessage,
+            details: err.message
+        }
+    };
+    res.status(500).json(response);
 });
 
 app.listen(port, () => {
---
+++ src/server/serverUtils.js
@@ -0,0 +1,38 @@
+const { Configuration, OpenAIApi } = require("openai");
+const config = require('./apiReqConfig.json'); // Load OpenAI configuration
+
+// generates the prompt for OpenAI based on code and logs
+const promptGenerator = (code, logs) => {
+    if (!code || !logs)
+        throw new Error("Code or Logs cannot be empty!");
+
+    // Concatenate prompts from configuration string and user inputs
+    return config.ACT.STEP_BY_STEP.concat(config.PRE_CODE_PROMPT, code, config.PRE_LOG_PROMPT, logs);
+    
+};
+
+// Send a prompt to OpenAI and get a completion
+const postPrompt = async (prompt) => {
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY, // Get API key from environment variables
+    });
+    const openai = new OpenAIApi(configuration);
+
+    // Create a chat completion request to OpenAI API
+    const response = await openai.createChatCompletion({
+        model: config.GPT.MODEL,
+        messages: [{"role":config.ROLE,"content":prompt}],
+        temperature: config.GPT.TEMPERATURE,
+        max_tokens: config.GPT.MAX_TOKENS,
+        top_p: config.GPT.TOP_P,
+        frequency_penalty: config.GPT.FREQUENCY_PENALTY,
+        presence_penalty: config.GPT.PRESENCE_PENALTY,
+    }); 
+
+    return response.data.choices[0].message.content;
+};
+
+module.exports = {
+    promptGenerator,
+    postPrompt,
+};
