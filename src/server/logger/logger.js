7334bd306d6d1e24d4ece7659e1e94b8fbd98773
Liran <liran.alb@gmail.com>
2023-09-16 20:16:24 +0300

    Add consts for configuration variable and reformat file

--- src/server/logger/logger.js
+++ src/server/logger/logger.js
@@ -4,9 +4,12 @@ const { combine, timestamp, printf, json } = format;
 const DEV_LOGGER_PATH = process.env.DEV_LOGGER_PATH || 'src/server/logger/logger_dev.log';
 const PROD_LOGGER_PATH = process.env.PROD_LOGGER_PATH || 'src/server/logger/logger_prod.log';
 
-// Define log levels for prod and dev
-const devLogLevel = 'debug';
-const prodLogLevel = 'info';
+// Define log levels for prod and dev, log file max size
+// And number of backup log files to keep
+const DEV_LOG_LEVEL = 'debug';
+const MAX_LOG_SIZE = 10485760;
+const BACKUP_TO_KEEP = 3;
+const PROD_LOG_LEVEL = 'info';
 
 // custom format for dev
 const logFormat = printf(({ level, message, timestamp, stack }) => {
@@ -14,48 +17,48 @@ const logFormat = printf(({ level, message, timestamp, stack }) => {
 });
 
 const logger = createLogger({
-  level: process.env.NODE_ENV === 'production' ? prodLogLevel : devLogLevel,
-  format: combine(
-    timestamp(),
-    format.errors({ stack: true }),
-    logFormat
-  ),
-  transports: [
-    new transports.Console({
-      level: devLogLevel,
-      format: combine(
+	level: process.env.NODE_ENV === 'production' ? PROD_LOG_LEVEL : DEV_LOG_LEVEL,
+	format: combine(
+    	timestamp(),
+    	format.errors({ stack: true }),
+    	logFormat
+	),
+	transports: [
+    	new transports.Console({
+		level: DEV_LOG_LEVEL,
+	format: combine(
         timestamp(),
         format.colorize(),
         logFormat
-      ),
+		),
     }),
     new transports.File({
-      filename: process.env.NODE_ENV === 'production' ? PROD_LOGGER_PATH : DEV_LOGGER_PATH,
-      level: process.env.NODE_ENV === 'production' ? prodLogLevel : devLogLevel,
-      format: combine(
-        timestamp(),
-        logFormat
+	filename: process.env.NODE_ENV === 'production' ? PROD_LOGGER_PATH : DEV_LOGGER_PATH,
+	level: process.env.NODE_ENV === 'production' ? PROD_LOG_LEVEL : DEV_LOG_LEVEL,
+	format: combine(
+		timestamp(),
+		logFormat
       ),
-      maxsize: 10485760, // limit log file to 10 MB 
-      maxFiles: 3,       // limit backup log files to 3
+		maxsize: MAX_LOG_SIZE, // limit log file to 10 MB 
+		maxFiles: BACKUP_TO_KEEP,       // limit backup log files to 3
     }),
   ],
 });
 
 // Log format for PRODUCTION: JSON
 if (process.env.NODE_ENV === 'production') {
-  logger.add(
-    new transports.File({
-      filename: PROD_LOGGER_PATH,
-      level: prodLogLevel,
-      format: combine(
-        timestamp(),
-        json()
-      ),
-      maxsize: 10485760, // limit log file to 10 MB 
-      maxFiles: 3,       // limit backup log files to 3
-    })
-  );
+	logger.add(
+	new transports.File({
+		filename: PROD_LOGGER_PATH,
+		level: PROD_LOG_LEVEL,
+		format: combine(
+			timestamp(),
+			json()
+		),
+		maxsize: MAX_LOG_SIZE, // limit log file to 10 MB 
+		maxFiles: BACKUP_TO_KEEP,       // limit backup log files to 3
+		})
+	);
 }
 
 module.exports = logger;
