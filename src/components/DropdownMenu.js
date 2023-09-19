588f3e71a51dbe3f15e394e887ea4ab6dbfd83bb
Liran <liran.alb@gmail.com>
2023-09-19 21:43:16 +0300

    add style and text to dropmenu

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-const config = require('../config/apiReqConfig.json');
+import './DropdownMenu.css'; // Import a custom CSS file for styling (create this file if it doesn't exist)
+const config = require('../config/prompt_config.json');
 
 const DropdownMenu = () => {
-  const actRoles = Object.keys(config.ACT);
+  const actRoles = Object.keys(config.CODE_GUIDANCE);
   const [selectedOption, setSelectedOption] = useState(actRoles[0]); // Set the default selected option
 
   const handleOptionChange = (event) => {
@@ -10,14 +11,21 @@ const DropdownMenu = () => {
   };
 
   return (
-    <div>
-      <select value={selectedOption} onChange={handleOptionChange}>
-        {actRoles.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
-      </select>
+    <div className="center">
+      <div className="dropdown-container">
+        <h1 className="dropdown-label">Choose a filter:   </h1>
+        <select
+          className="select-style"
+          value={selectedOption}
+          onChange={handleOptionChange}
+        >
+          {actRoles.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      </div>
     </div>
   );
 };
